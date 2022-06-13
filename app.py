277096d818d66469626c637474eda1b812b26fa0
Kiara <michelle_vong@brown.edu>
2022-06-13 12:11:42 -1000

    heroku

--- app.py
+++ app.py
@@ -11,20 +11,6 @@ base = Blueprint('base', __name__, template_folder='templates')
 @base.route('/')
 def index():
     return render_template('index.html')
-    # base_page_html = f"""
-    # <html>
-    #    <body>
-    #     <h1>Hot dog? Or not...</h1>
-    #     Upload your image here:
-    #       <form action="{server_address}/result" method="POST" 
-    #          enctype="multipart/form-data">
-    #          <input type="file" name="file" />
-    #          <input type="submit"/>
-    #       </form>
-    #    </body>
-    # </html>
-    # """
-    # return base_page_html
 
 # Page presenting results and option to try again
 @base.route('/result', methods=['GET','POST'])
