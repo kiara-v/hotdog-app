948a8e2031f4582204d60d3572cc9b034ecfa3e5
Kiara <michelle_vong@brown.edu>
2022-06-13 14:10:55 -1000

    working cries

--- hotdog_app.py
+++ hotdog_app.py
@@ -50,5 +50,6 @@ def hotdog_result():
 
     return render_template('result.html', result = result)
 
+port = int(os.environ.get("PORT", 5000))
 if __name__ == '__main__':
- 	app.run()
+ 	app.run(host='0.0.0.0', port=port, debug=True)
